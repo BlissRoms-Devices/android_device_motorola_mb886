fd37a2d9b925cd91ce4c80adcbe14a795fd959c3
Nathan Prevette <nprevette420@gmail.com>
2016-06-03 14:28:40 -0700

    Blissify

--- README.md
+++
@@ -1,4 +0,0 @@
-Device configuration for the Motorola Atrix HD (dinara/qinara)
-===============================
-
-Copyright 2015 - The CyanogenMod Project
--- bliss.mk
+++ bliss.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/qcom-common/overlay-cm
 
@@ -11,12 +11,13 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
-PRODUCT_NAME := cm_mb886
+PRODUCT_NAME := bliss_mb886
 TARGET_VENDOR_PRODUCT_NAME := MB886_att
 TARGET_VENDOR_DEVICE_NAME := qinara
 
 $(call inherit-product, device/motorola/mb886/full_mb886.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=mb886 \
     BUILD_FINGERPRINT=motorola/MB886_att/qinara:4.1.1/9.8.0Q-97_MB886_FFW-20/27:user/release-keys \
     PRIVATE_BUILD_DESC="motorola/MB886_att/qinara:4.1.1/9.8.0Q-97_MB886_FFW-20/27:user/release-keys"
